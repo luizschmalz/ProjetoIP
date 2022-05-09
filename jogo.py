8e879d137abe09b9917d79a0835f434d9a358f47
Luiz Schmalz <102548215+luizschmalz@users.noreply.github.com>
2022-05-09 01:10:04 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -9,7 +9,9 @@ pg.init()
 largura = 800
 altura = 600
 tela = pg.display.set_mode((largura, altura))
-fundo = pg.image.load("fundo.jpg")
+fundo = pg.image.load("fundo.png")
+start = False
+fundo_start = pg.image.load("tela inicio.png")
 relogio = pg.time.Clock()
 z = 0
 w = 0
@@ -18,7 +20,7 @@ pronto = False
 crachas = 3
 debuff = 0
 rodando = True
-musica_fundo = pg.mixer.music.load("musicafundo.mp3.mp3")
+musica_fundo = pg.mixer.music.load("musicafundo.mp3")
 pg.mixer.music.play(-1)
 
 
@@ -38,8 +40,6 @@ class Cracha(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.y = randint(50, 550)
         self.rect.x = randint(50, 750)
-
-
 
 class Gota(pg.sprite.Sprite):
     def __init__(self):
@@ -68,7 +68,6 @@ class Gota(pg.sprite.Sprite):
         self.rect.y = w
         self.rect.x = z
 
-
 class Reidograd(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
@@ -94,12 +93,28 @@ class Reidograd(pg.sprite.Sprite):
         if self.atual >= 8:
             self.atual = 0
         self.image = self.sprites[self.atual]
-        self.image = pg.transform.scale(self.image, (64 * 1.5, 64 * 1.5))
-        if self.rect.topright[0] <= largura+30:
-            self.rect.x += 10
+        self.image = pg.transform.scale(self.image, (int(64 * 1.5), int(64 * 1.5)))
+
+        if crachas == 0:
+            if self.rect.x > z:
+                self.rect.x += 4
+            elif self.rect.x < z:
+                self.rect.x -= 4
+
+            if self.rect.y > w:
+                self.rect.y += 4
+            elif self.rect.y < w:
+                self.rect.y -= 4
         else:
-            self.rect.x = 0
-            self.rect.y = randint(50,600)
+            if self.rect.x > z:
+                self.rect.x -= 4
+            elif self.rect.x < z:
+                self.rect.x += 4
+
+            if self.rect.y > w:
+                self.rect.y -= 4
+            elif self.rect.y < w:
+                self.rect.y += 4
 
 class Marmita(pg.sprite.Sprite):
     def __init__(self):
@@ -108,8 +123,8 @@ class Marmita(pg.sprite.Sprite):
         self.sprites.append(pg.image.load('almirante.png'))
         self.image = self.sprites[0]
         self.rect = self.image.get_rect()
-        self.rect.x = randint(50,750)
-        self.rect.y = randint(50,600)
+        self.rect.x = randint(50,700)
+        self.rect.y = randint(50,500)
         self.mask = pg.mask.from_surface(self.image)
 
 
@@ -135,101 +150,111 @@ comida.add(marmita)
 
 
 while True:
-    tela.blit(fundo, (0, 0))
-    relogio.tick(30)
-    mensagem4 = f"Crachas Restantes: {crachas}"
-    texto4 = fonte.render(mensagem4, False, (0, 0, 0))
-    tela.blit(texto4, (450, 50))
-    sprites.draw(tela)
-    for event in pg.event.get():
-        if event.type == QUIT:
-            pg.quit()
-            exit()
-    if rodando:
-        if debuff == 0:
-            if pg.key.get_pressed()[K_a]:
-                z = z - 10
-            if pg.key.get_pressed()[K_d]:
-                z = z + 10
-            if pg.key.get_pressed()[K_w]:
-                w = w - 10
-            if pg.key.get_pressed()[K_s]:
-                w = w + 10
-        elif debuff == 1:
-            if pg.key.get_pressed()[K_a]:
-                z = z - 7
-            if pg.key.get_pressed()[K_d]:
-                z = z + 7
-            if pg.key.get_pressed()[K_w]:
-                w = w - 7
-            if pg.key.get_pressed()[K_s]:
-                w = w + 7
-        elif debuff == 2:
-            if pg.key.get_pressed()[K_a]:
-                z = z - 5
-            if pg.key.get_pressed()[K_d]:
-                z = z + 5
-            if pg.key.get_pressed()[K_w]:
-                w = w - 5
-            if pg.key.get_pressed()[K_s]:
-                w = w + 5
-        elif debuff == 3:
-            if pg.key.get_pressed()[K_a]:
-                z = z - 2
-            if pg.key.get_pressed()[K_d]:
-                z = z + 2
-            if pg.key.get_pressed()[K_w]:
-                w = w - 2
-            if pg.key.get_pressed()[K_s]:
-                w = w + 2
-    else:
-        pass
-
-    colisao_cracha = pg.sprite.spritecollide(gota, coletaveis, False, pg.sprite.collide_mask)
-    if colisao_cracha:
-        crachas -= 1
-        debuff += 1
-        if crachas == 0:
-            colisao_cracha = pg.sprite.spritecollide(gota, coletaveis, True, pg.sprite.collide_mask)
-            pronto = True
-        else:
-            coletaveis.draw(tela)
-            coletaveis.update()
-    else:
-        pass
-
-    colisao_comida = pg.sprite.spritecollide(gota, comida, True, pg.sprite.collide_mask)
-    if colisao_comida:
-        if debuff > 0:
-            debuff -=1
+    if start:
+        tela.blit(fundo, (0, 0))
+        relogio.tick(30)
+        mensagem4 = f"Crachas Restantes: {crachas}"
+        texto4 = fonte.render(mensagem4, False, (255, 255, 255))
+        tela.blit(texto4, (480, 50))
+        sprites.draw(tela)
+        for event in pg.event.get():
+            if event.type == QUIT:
+                pg.quit()
+                exit()
+        if rodando:
+            if debuff == 0:
+                if pg.key.get_pressed()[K_a]:
+                    z = z - 10
+                if pg.key.get_pressed()[K_d]:
+                    z = z + 10
+                if pg.key.get_pressed()[K_w]:
+                    w = w - 10
+                if pg.key.get_pressed()[K_s]:
+                    w = w + 10
+            elif debuff == 1:
+                if pg.key.get_pressed()[K_a]:
+                    z = z - 7
+                if pg.key.get_pressed()[K_d]:
+                    z = z + 7
+                if pg.key.get_pressed()[K_w]:
+                    w = w - 7
+                if pg.key.get_pressed()[K_s]:
+                    w = w + 7
+            elif debuff == 2:
+                if pg.key.get_pressed()[K_a]:
+                    z = z - 5
+                if pg.key.get_pressed()[K_d]:
+                    z = z + 5
+                if pg.key.get_pressed()[K_w]:
+                    w = w - 5
+                if pg.key.get_pressed()[K_s]:
+                    w = w + 5
+            elif debuff == 3:
+                if pg.key.get_pressed()[K_a]:
+                    z = z - 2
+                if pg.key.get_pressed()[K_d]:
+                    z = z + 2
+                if pg.key.get_pressed()[K_w]:
+                    w = w - 2
+                if pg.key.get_pressed()[K_s]:
+                    w = w + 2
         else:
             pass
+
+        colisao_cracha = pg.sprite.spritecollide(gota, coletaveis, False, pg.sprite.collide_mask)
+        if colisao_cracha:
+            crachas -= 1
+            debuff += 1
+            if crachas == 0:
+                colisao_cracha = pg.sprite.spritecollide(gota, coletaveis, True, pg.sprite.collide_mask)
+                pronto = True
+            else:
+                coletaveis.draw(tela)
+                coletaveis.update()
+        else:
+            pass
+
+        colisao_comida = pg.sprite.spritecollide(gota, comida, True, pg.sprite.collide_mask)
+        if colisao_comida:
+            if debuff > 0:
+                debuff -=1
+            else:
+                pass
+        else:
+            pass
+
+
+        colisao_inimigo = pg.sprite.spritecollide(gota, inimigos, False, pg.sprite.collide_mask)
+        if colisao_inimigo and not pronto:
+            mensagem1 = "Você Perdeu!"
+            rodando = False
+            texto1 = fonte.render(mensagem1, True, (0, 0, 0))
+            tela.blit(fundo, (0, 0))
+            sprites.draw(tela)
+            tela.blit(texto1, (280,200))
+        if colisao_inimigo and pronto:
+            mensagem2 = "Parabéns, agora você se tornou o novo Rei do Grad"
+            rodando = False
+            texto2 = fonte.render(mensagem2, True, (0, 0, 0))
+            tela.blit(fundo, (0, 0))
+            tela.blit(texto2, (50, 200))
+            vencedor.draw(tela)
+            vencedor.update()
+
+        elif not colisao_inimigo:
+            vencedor.update()
+            inimigos.update()
     else:
-        pass
-
-
-    colisao_inimigo = pg.sprite.spritecollide(gota, inimigos, False, pg.sprite.collide_mask)
-    if colisao_inimigo and not pronto:
-        mensagem1 = "Você Perdeu!"
-        rodando = False
-        texto1 = fonte.render(mensagem1, True, (0, 0, 0))
-        tela.blit(fundo, (0, 0))
-        sprites.draw(tela)
-        tela.blit(texto1, (280,200))
-    if colisao_inimigo and pronto:
-        mensagem2 = "Parabéns, agora você se tornou o novo Rei do Grad"
-        rodando = False
-        texto2 = fonte.render(mensagem2, True, (0, 0, 0))
-        tela.blit(fundo, (0, 0))
-        tela.blit(texto2, (50, 200))
-        vencedor.draw(tela)
-        vencedor.update()
-
-    elif not colisao_inimigo:
-        vencedor.update()
-        inimigos.update()
-
-
+        relogio.tick(30)
+        tela.blit(fundo_start, (0,0))
+        for event in pg.event.get():
+            if event.type == pg.KEYDOWN:
+                start = True
+            else:
+                pass
+            if event.type == QUIT:
+                pg.quit()
+                exit()
 
 
 
